71aca4d600c357610e7741ceb82d5ae41d400adb
[Kemmie001] <[Oluwakemisola757@gmail.com]>
2021-11-24 08:46:03 +0100

    have done the mobile side bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,21 @@
+import React, {useState, useEffect} from 'react'
 import logo from '../assets/logo.png'
 import { FaBars } from 'react-icons/fa'
-function navbar() {
+
+import '../index.css'
+export default function Navbar() {
+	const [toggleMenu, setToggleMenu] = useState(false)
+	const [screenWidth, setScreenWidth] = useState
+	(window.innerWidth)
+ 	const toggleNav = () => {
+		setToggleMenu(!toggleMenu)
+	}
+	useEffect(() => {
+		const changeWidth = () =>{
+			setScreenWidth(window.innerWidth)
+		}
+		window.addEventListener('resize', changeWidth)
+	})
 	return (
 		<div>
 			<div className="nav">
@@ -8,9 +23,11 @@ function navbar() {
 					<div className="nav-img">
 						<img src={logo} alt="" />
 					</div>
-					<FaBars className="bar" style={{ color: "#ffffff", fontSize: "32px", cursor: "pointer", alignSelf: "center" }} />
+					<FaBars onClick={toggleNav} className="bar" style={{ color: "#ffffff", fontSize: "32px", cursor: "pointer", alignSelf: "center" }} />
 				</div>
-				<div className="sidebar-backdrop">
+				
+				{(toggleMenu || screenWidth > 768) && (
+					<div onClick={toggleNav} className="sidebar-backdrop">
 				<ul className="">
 					<li className=""><a>Home</a></li>
 					<li className=""><a>Invest</a></li>
@@ -20,11 +37,12 @@ function navbar() {
 					<li className=""><a>FAQ</a></li>
 					<li className=""><a>Contact</a></li>
 				</ul>
-
 				</div>
+				)}
+				
 			</div>
 		</div>
 	)
 }
 
-export default navbar
+
